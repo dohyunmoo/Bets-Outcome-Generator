e2c076a94994457b54b995afe20490bc5d070d3c
Dohyun Moon <d6moon@uwaterloo.ca>
2023-02-25 23:37:49 -0500

    removed some files

--- entry.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-class Entry(tk.Entry):
-    def __init__(self, main=None, other=None, **kwargs):
-        super().__init__(main, **kwargs)
-        self.other = other
-
-        self.bind()
